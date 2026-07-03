98384985f1469c0032abf87f448b3dea02353115
Julio <juliocesar.gm6000@gmail.com>
2026-07-03 14:56:13 -0300

    feat: DbContext da aplicação criado

--- src/AgiliServiceDesk/AgiliServiceDesk/Data/AppDbContext.cs
+++ src/AgiliServiceDesk/AgiliServiceDesk/Data/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace AgiliServiceDesk.Data
+﻿using AgiliServiceDesk.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgiliServiceDesk.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Chamado> Chamados { get; set; }
     }
 }
